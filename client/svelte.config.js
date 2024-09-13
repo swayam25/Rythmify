bdf12d0f929ae65381ef2fcb907460807556b392
swayam25 <178068815+swayam25@users.noreply.github.com>
2024-09-13 16:27:28 +0530

    Fix svelte adapter
    - Changed `@sveltejs/adapter-static` to `@sveltejs/adapter-node`

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
     kit: { adapter: adapter() },
     preprocess: vitePreprocess()
-}
+};
 
 export default config;
