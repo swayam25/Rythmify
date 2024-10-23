9290105c18b9ef24954e60a2f2e4d1636a79f0c1
swayam25 <178068815+swayam25@users.noreply.github.com>
2024-10-23 08:48:33 +0530

    Refactor image sizes in trending page

--- client/src/routes/(app)/trending/+page.svelte
+++ client/src/routes/(app)/trending/+page.svelte
@@ -15,7 +15,7 @@
     {#if trend.length === 0}
         {#each Array.from({ length: 20 }, (_, i) => i + 1) as i}
             <div class="flex flex-col rounded-token w-fit p-5 duration-200 items-center justify-center animate-pulse">
-                <div class="h-32 lg:h-52 w-32 lg:w-52 rounded-token bg-surface-700/40" />
+                <div class="h-32 lg:h-40 w-32 lg:w-40 rounded-token bg-surface-700/40" />
                 <div class="mt-5">
                     <div class="h-5 w-32 rounded-token bg-surface-700/40" />
                     <div class="h-5 w-32 rounded-token bg-surface-700/40 mt-2" />
@@ -30,7 +30,7 @@
                 transition:fade
                 class="hover:bg-surface-200/50 hover:dark:bg-surface-700/40 hover:shadow-lg flex flex-col rounded-token w-fit p-5 duration-200 items-center justify-center"
             >
-                <img src={song.cover} alt={song.title} class="h-32 lg:h-52 w-32 lg:w-52 object-cover rounded-token" />
+                <img src={song.cover} alt={song.title} class="h-32 lg:h-40 w-32 lg:w-40 object-cover rounded-token" />
                 <div class="mt-5 max-w-[8rem] overflow-hidden text-center">
                     <h1 title={song.title} class="text-base lg:text-lg font-bold truncate">{song.title}</h1>
                     <p title={song.artist} class="text-sm text-primary-500-400-token truncate">{song.artist}</p>
